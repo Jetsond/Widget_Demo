5e6014e4f4d227bf97f7ef1bf2e0002fcd55564e
superMan <zhangc1993@icloud.com>
2025-08-20 14:30:19 +0800

    fix creat project

---
+++ widget_demo/widget_demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  widget_demo
+//
+//  Created by chao on 2025/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
