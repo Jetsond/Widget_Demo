f4054ac6866c7e33c2d284c4ade70b8acd7f6f81
superMan <zhangc1993@icloud.com>
2025-12-19 17:28:36 +0800

    fix new tabbar home my vc

---
+++ widget_demo/widget_demo/ZCTabViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ZCTabViewController.swift
+//  widget_demo
+//
+//  Created by chao on 2025/12/19.
+//
+
+import UIKit
+
+class ZCTabViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupViewControllers()
+    }
+    
+    private func setupViewControllers (){
+        
+    }
+
+}
