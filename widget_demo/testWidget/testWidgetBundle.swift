f5b14fd54b7dfe5aea6e5b13f48e73f3ad7de781
superMan <zhangc1993@icloud.com>
2025-12-19 15:02:23 +0800

    添加小组件

---
+++ widget_demo/testWidget/testWidgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  testWidgetBundle.swift
+//  testWidget
+//
+//  Created by chao on 2025/8/20.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct testWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        testWidget()
+        testWidgetControl()
+        testWidgetLiveActivity()
+    }
+}
